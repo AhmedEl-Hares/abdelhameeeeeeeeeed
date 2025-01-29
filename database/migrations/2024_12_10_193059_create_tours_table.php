e08b63f6000670fa3dc034e97c7073332384f83f
Ahmed-Adel-Ahmed <155266329+Ahmed-Adel-Ahmed@users.noreply.github.com>
2025-01-29 15:44:35 +0200

    Update 2024_12_10_193059_create_tours_table.php foreign key update

--- database/migrations/2024_12_10_193059_create_tours_table.php
+++ database/migrations/2024_12_10_193059_create_tours_table.php
@@ -14,16 +14,19 @@ return new class extends Migration
         Schema::create('tours', function (Blueprint $table) {
             $table->id('Tour_Id')->primary();
             $table->string('Name');
-            $table->id('Tourist_Id')->foreign('Tourist_id')->references('Tourist_Id')->on('tourists');
-            $table->id('Guide_Id')->foreign('Guide_id')->references('Guide_Id')->on('guides');
             $table->string('Status')->default('Pending'); //Pending, Approved, Rejected
-            $table->id('Transportation_Id')->foreign('Transportation_Id')->references('Transportation_Id')->on('transportations');
             $table->integer('Number_of_Adults')->default(1);
             $table->integer('Number_of_kids')->nullable();
             $table->date('Date');
             $table->time('Start_Time');
             $table->time('End_Time');
             $table->json('Locations');
+
+            // Define foreign keys
+            $table->foreign('Tourist_Id')->references('Tourist_Id')->on('tourists')->onDelete('cascade');
+            $table->foreign('Guide_id')->references('Guide_Id')->on('guides')->onDelete('cascade');
+            $table->foreign('Transportation_Id')->references('Transportation_Id')->on('transportations')->onDelete('cascade');
+        });
         });
     }
 
