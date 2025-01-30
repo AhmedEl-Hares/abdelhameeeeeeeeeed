ee26fcab871889a55f69161d7d9a70296cf1335e
EL-HARES <ahmedelhares.work@gmail.com>
2025-01-30 11:48:22 +0200

    adding nullable and notNull  constrains to tours table

--- database/migrations/2024_12_10_193059_create_tours_table.php
+++ database/migrations/2024_12_10_193059_create_tours_table.php
@@ -13,17 +13,17 @@ return new class extends Migration
     {
         Schema::create('tours', function (Blueprint $table) {
             $table->id('Tour_Id')->primary();
-            $table->string('Name');
+            $table->string('Name')->required();
             $table->string('Status')->default('Pending'); //Pending, Approved, Rejected
             $table->integer('Number_of_Adults')->default(1);
             $table->integer('Number_of_kids')->nullable();
-            $table->unsignedBigInteger('Tourist_Id');
-            $table->unsignedBigInteger('Guide_Id');
-            $table->unsignedBigInteger('Transportation_Id');
+            $table->unsignedBigInteger('Tourist_Id')->required();
+            $table->unsignedBigInteger('Guide_Id')->nullable();
+            $table->unsignedBigInteger('Transportation_Id')->nullable();
             $table->date('Date');
             $table->time('Start_Time');
             $table->time('End_Time');
-            $table->json('Locations');
+            $table->json('Locations')->nullable();
 
             // Define foreign keys
             $table->foreign('Tourist_Id')->references('Tourist_Id')->on('tourists')->onDelete('cascade');
